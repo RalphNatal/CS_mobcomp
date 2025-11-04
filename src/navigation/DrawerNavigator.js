79809a8fa1d6bfbe7cafbe9a70960a3411ae483c
NeonsCookies <leejerom079@gmail.com>
2025-11-05 00:36:49 +0800

    Update DrawerNavigator.js

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/tempHomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import CustomDrawer from '../components/CustomDrawer';
-import theme from '../styles/theme';
 import CategoryScreen from '../screens/CategoryScreen';
+import { ThemeContext } from '../../App';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
+  const { currentTheme } = useContext(ThemeContext);
+
   return (
     <Drawer.Navigator
       drawerContent={(props) => <CustomDrawer {...props} />}
       screenOptions={{
-        headerStyle: { backgroundColor: theme.colors.primary },
+        headerStyle: { backgroundColor: currentTheme.colors.primary },
         headerTintColor: '#fff',
-        drawerActiveBackgroundColor: theme.colors.primary,
+        drawerActiveBackgroundColor: currentTheme.colors.primary,
         drawerActiveTintColor: '#fff',
-        drawerInactiveTintColor: theme.colors.text,
+        drawerInactiveTintColor: currentTheme.colors.text,
         drawerType: 'slide',
       }}
     >
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
       <Drawer.Screen name="Settings" component={SettingsScreen} />
+      {/* Add other screens as needed */}
     </Drawer.Navigator>
   );
 }

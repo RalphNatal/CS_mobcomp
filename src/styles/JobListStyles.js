297e0226989c417755ccbadab4810515767b4041
NeonsCookies <leejerom079@gmail.com>
2025-11-12 00:50:42 +0800

    Update JobListStyles.js

--- src/styles/JobListStyles.js
+++ src/styles/JobListStyles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-export const jobListStyles = (theme) => StyleSheet.create({
+export const jobListStyles = (theme, fontSizeMultiplier = 1, dyslexicEnabled = false) => StyleSheet.create({
   container: {
     flex: 1,
     padding: theme.spacing.m,
